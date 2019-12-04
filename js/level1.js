7cdb1832754bff8aa3894b7edf46edf38b598809
Bartomeu <bartomeu.puig@enti.cat>
2019-12-04 17:00:45 +0100

    Camera Lerp y escaleras (primera mitad)

--- js/level1.js
+++ js/level1.js
@@ -9,8 +9,6 @@ platformer.level1 ={
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.physics.arcade.gravity.y = gameOptions.heroGravity;
         
-        this.game.world.setBounds(0,0,5120,1200);
-        
     },
     preload:function(){
         var ruta = 'assets/sprites/';
@@ -41,8 +39,13 @@ platformer.level1 ={
         this.walls.setScale(0.5, 0.5);
         this.death = this.map.createLayer('Death');
         this.death.setScale(0.5, 0.5);
+        this.steps = this.map.createLayer('Steps');
+        this.steps.setScale(0.5, 0.5);
+        this.lerp = this.map.createLayer('lerp');
+        this.lerp.setScale(0.5, 0.5);
         this.map.setCollisionBetween(1,1,true,'Walls');
         this.map.setCollisionBetween(1,1,true,'Death');
+        this.map.setCollisionBetween(1,1,true,'Steps');
     
 
         this.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -55,25 +58,41 @@ platformer.level1 ={
         this.megaman = this.game.add.sprite(100, 100, 'megaman', 3);
         this.megaman.anchor.setTo(0.5, 0);
         this.game.physics.arcade.enable(this.megaman);
-        this.megaman.body.setSize(this.megaman.body.width - 5, this.megaman.body.height, 2.5, 0);
+        this.megaman.body.setSize(this.megaman.body.width - 9, this.megaman.body.height, 4.5, 0);
         this.megaman.animations.add('run', Phaser.Animation.generateFrameNames('run', 1, 3), 5, true);
         this.shootRun = this.megaman.animations.add('shoot_run', Phaser.Animation.generateFrameNames('shoot_run', 1, 3), 5, true);
+        
+        this.stage = 1;
         
         this.loadBullets();
         this.space.onDown.add(this.createBullet,this);
         
         this.shoot = this.game.add.audio('shoot');
         
+        this.lerping = false;
+        this.lerpValue = 0;
         this.camera.follow(this.megaman,Phaser.Camera.FOLLOW_PLATFORMER);
-              
+        
+        this.game.world.setBounds(0,0,this.map.getTile(127, 9).worldX + this.map.getTile(127, 9).width,1200);
+        
+        this.loadStairs();
+        
+        this.megaman.position.set(this.map.getTile(120, 22, 'lerp').worldX, this.map.getTile(120, 22, 'lerp').worldY, 'patron');
     },
     update:function(){
         this.game.physics.arcade.collide(this.megaman,this.walls);
-        if(this.game.physics.arcade.collide(this.megaman,this.death)) 
-        {
-            this.megaman.reset(100,100);
-        }
+        this.checkLava();
+        this.checkStage();
+        if(this.lerping == false) this.checkMegamanMovement();
         
+        
+    },
+    checkMegamanMovement:function()
+    {
+        this.checkStairs();
+        if(this.stair == -1)
+        {
+            this.megaman.body.allowGravity = true;
         if(this.cursors.left.isDown){
             this.megaman.body.velocity.x = -gameOptions.heroSpeed;
             if(this.megaman.body.blocked.down){
@@ -88,6 +107,7 @@ platformer.level1 ={
                 if(this.shooting == true) this.megaman.animations.play('shoot_run');
                 else if(!this.shootRun.isPlaying)this.megaman.animations.play('run');
             }
+            
             this.megaman.scale.x = 1;
         }else{
             this.megaman.body.velocity.x = 0;
@@ -97,7 +117,7 @@ platformer.level1 ={
                 if(this.megaman.frame != 9)this.megaman.frame=3;
             }
         }
-        if(this.cursors.up.isDown && this.megaman.body.blocked.down&&this.cursors.up.downDuration(1)){
+        if(this.cursors.up.isDown &&this.cursors.up.downDuration(1)){ // && this.megaman.body.blocked.down
             this.megaman.body.velocity.y = -gameOptions.heroJump;
             this.megaman.animations.stop();
         }
@@ -110,6 +130,32 @@ platformer.level1 ={
             }
         }
         this.shooting = false;
+        }
+        else{
+            this.megaman.body.allowGravity = false;           
+            if(this.cursors.up.isDown){
+            this.megaman.body.velocity.y = -gameOptions.heroSpeed;
+        }
+            else if(this.cursors.down.isDown){
+            this.megaman.body.velocity.y = gameOptions.heroSpeed;
+            }
+            else {
+                this.megaman.body.velocity.set(0, 0);
+            }
+            if(this.megaman.body.blocked.down)
+                {
+                    if(this.cursors.left.isDown){
+            this.megaman.body.velocity.x = -gameOptions.heroSpeed;
+        }
+            else if(this.cursors.right.isDown){
+            this.megaman.body.velocity.x = gameOptions.heroSpeed;
+            }
+                }
+            else {
+                this.megaman.position.x = this.stairs[this.stair].centerX + 2.5;
+            }
+            this.megaman.scale.x = 1;
+        }
     },
     loadBullets:function(){
         this.bullets=this.add.group();
@@ -134,8 +180,133 @@ platformer.level1 ={
     render:function()
     {
         this.game.debug.body(this.megaman);
-    }
+    },
     
+    checkLava:function()
+    {
+        if(this.game.physics.arcade.collide(this.megaman,this.death)) 
+        {
+            this.megaman.reset(100,100);
+        }
+    },
+    checkStage:function()
+    {
+        console.log(this.lerpValue);
+        switch(this.stage)
+            {
+                case 1:
+                    if (this.megaman.position.y > this.map.getTile(125, 15, 'lerp').worldY && this.lerping == false)
+                        {
+                            this.lerpPatron = this.game.add.sprite(this.map.getTile(120, 22, 'lerp').worldX, this.map.getTile(120, 22, 'lerp').worldY, 'patron')
+                            this.camera.follow(this.lerpPatron,Phaser.Camera.FOLLOW_LOCKON, this.lerpValue, this.lerpValue);
+                            this.lerping = true;
+                            this.megaman.body.allowGravity = false;
+                            this.megaman.body.velocity.set(0, 0);
+                        }
+                    else if(this.megaman.position.y > this.map.getTile(125, 15, 'lerp').worldY)
+                        {
+                            this.lerpValue += 0.005;
+                            this.camera.follow(this.lerpPatron,Phaser.Camera.FOLLOW_LOCKON, this.lerpValue, this.lerpValue);
+                            if(this.lerpValue >= 0.3) {
+                                this.stage = 2;
+                                this.lerpValue = 0;
+                                this.lerping = false;
+                                this.megaman.body.allowGravity = true;
+                            }
+                        }
+                    break;
+                case 2:
+                    if (this.megaman.position.y < this.map.getTile(125, 15, 'lerp').worldY &&
+                        this.megaman.position.x > this.map.getTile(125, 15, 'lerp').worldX && this.lerping == false)
+                        {
+                            this.lerpPatron = this.megaman;
+                            this.camera.follow(this.lerpPatron,Phaser.Camera.FOLLOW_LOCKON, this.lerpValue, this.lerpValue);
+                            this.lerping = true;
+                            this.megaman.body.allowGravity = false;
+                            this.megaman.body.velocity.set(0, 0);
+                        }
+                    else if(this.megaman.position.y < this.map.getTile(125, 15, 'lerp').worldY &&
+                           this.megaman.position.x > this.map.getTile(125, 15, 'lerp').worldX)
+                        {
+                            
+                            this.lerpValue += 0.005;
+                            this.camera.follow(this.lerpPatron,Phaser.Camera.FOLLOW_LOCKON, this.lerpValue, this.lerpValue);
+                            if(this.lerpValue >= 0.3) {
+                                this.stage = 1;
+                                this.lerpValue = 0;
+                                this.lerping = false;
+                                this.megaman.body.allowGravity = true;
+                            }
+                        }
+                    
+                    else if (this.megaman.position.y > this.map.getTile(114, 29, 'lerp').worldY && this.lerping == false)
+                        {
+                            this.lerpPatron = this.megaman;
+                            this.camera.follow(this.lerpPatron,Phaser.Camera.FOLLOW_LOCKON, this.lerpValue, this.lerpValue);
+                            this.lerping = true;
+                            this.megaman.body.allowGravity = false;
+                            this.megaman.body.velocity.set(0, 0);
+                        }
+                    else if(this.megaman.position.y > this.map.getTile(114, 29, 'lerp').worldY)
+                        {
+                            
+                            this.lerpValue += 0.005;
+                            this.camera.follow(this.lerpPatron,Phaser.Camera.FOLLOW_LOCKON, this.lerpValue, this.lerpValue);
+                            if(this.lerpValue >= 0.3) {
+                                this.stage = 3;
+                                this.lerpValue = 0;
+                                this.lerping = false;
+                                this.megaman.body.allowGravity = true;
+                            }
+                        }
+                    break;
+                case 3:
+                    if (this.megaman.position.y < this.map.getTile(114, 29, 'lerp').worldY && this.lerping == false)
+                        {
+                            this.lerpPatron = this.game.add.sprite(this.map.getTile(120, 22, 'lerp').worldX, this.map.getTile(120, 22, 'lerp').worldY, 'patron');
+                            this.camera.follow(this.lerpPatron,Phaser.Camera.FOLLOW_LOCKON, this.lerpValue, this.lerpValue);
+                            this.lerping = true;
+                            this.megaman.body.allowGravity = false;
+                            this.megaman.body.velocity.set(0, 0);
+                        }
+                    else if(this.megaman.position.y < this.map.getTile(114, 29, 'lerp').worldY)
+                        {
+                            
+                            this.lerpValue += 0.005;
+                            this.camera.follow(this.lerpPatron,Phaser.Camera.FOLLOW_LOCKON, this.lerpValue, this.lerpValue);
+                            if(this.lerpValue >= 0.3) {
+                                this.stage = 2;
+                                this.lerpValue = 0;
+                                this.lerping = false;
+                                this.megaman.body.allowGravity = true;
+                            }
+                        }
+                    break;
+            }
+    },
+    loadStairs:function()
+    {
+         var stair_one = new Phaser.Rectangle(this.map.getTile(125, 11, 'Steps').worldX, this.map.getTile(125, 11, 'Steps').worldY, this.map.getTile(125, 11, 'Steps').width, this.map.getTile(125, 11, 'Steps').height * 14);
+        
+        var stair_two = new Phaser.Rectangle(this.map.getTile(114, 18, 'Steps').worldX, this.map.getTile(114, 18, 'Steps').worldY, this.map.getTile(114, 18, 'Steps').width, this.map.getTile(114, 18, 'Steps').height * 14);
+        
+        this.stairs = [];
+        this.stairs[0] = stair_one;
+        this.stairs[1] = stair_two;
+    },
+    checkStairs:function()
+    {
+        this.stair = -1;
+        
+        for (var i=0;i < this.stairs.length;i++)
+        {
+            if(this.stairs[i].intersectsRaw(this.megaman.position.x, this.megaman.position.x, this.megaman.position.y, this.megaman.position.y))
+            {
+                this.stair = i;
+            }
+        }
+        
+    }
 };
 
 
